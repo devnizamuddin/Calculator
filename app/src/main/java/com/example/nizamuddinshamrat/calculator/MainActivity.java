23291f6cbb9bb991ce48a76b64690330dfe1065b
Nizam Uddin Shamrat <nshamrat7@gmail.com>
2018-09-18 09:58:50 +0600

    Re_arrange Method

--- app/src/main/java/com/example/nizamuddinshamrat/calculator/MainActivity.java
+++ app/src/main/java/com/example/nizamuddinshamrat/calculator/MainActivity.java
@@ -10,11 +10,9 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String display = "";
-    private TextView displayTV,resultTV;
-    private String currentOperator = "";
-    private TextView deleteTv;
-    private  double result;
+    private TextView displayTV,resultTV,deleteTv;
+    private String display = "",currentOperator = "";
+    private  double result = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         resultTV = findViewById(R.id.resultTv);
         deleteTv = findViewById(R.id.deleteTv);
         displayTV.setText(display);
+
         deleteTv.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -34,16 +33,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
     public void updateScreen(){
         displayTV.setText(display);
+        resultTV.setText(String.valueOf(result));
     }
     public void clear(){
         display = "";
         currentOperator = "";
     }
 
+    public boolean isLastCharacterContainOperator(){
+        String lastCharacter = display.substring(display.length()-1);
+        if (lastCharacter.equals("+") || lastCharacter.equals("-") ||
+                lastCharacter.equals("×") || lastCharacter.equals("÷") || display.equals("")){
+
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public double operate(String numberOne, String numberTwo, String operator){
+        switch (operator){
+            case "+":
+                return Double.valueOf(numberOne)+Double.valueOf(numberTwo);
+            case "-":
+                return Double.valueOf(numberOne)-Double.valueOf(numberTwo);
+            case "×":
+                return Double.valueOf(numberOne)*Double.valueOf(numberTwo);
+            case "÷":
+                return Double.valueOf(numberOne)/Double.valueOf(numberTwo);
+        }
+        return 0;
+    }
+    public void getResult(){
+        String[]operation = display.split(Pattern.quote(currentOperator));
+        String firstNumber = operation[0];
+        String secondNumber = operation[operation.length-1];
+        if (result >0){
+            result = operate(String.valueOf(result),secondNumber,currentOperator);
+        }
+        else {
+            result = operate(operation[0],operation[1],currentOperator);
+        }
+        resultTV.setText(String.valueOf(result));
+        Toast.makeText(this, ""+operation[operation.length-1], Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickEqual(View view) {
+        getResult();
+    }
     public void onClickNumber(View view) {
         TextView textView = (TextView) view;
         display += textView.getText().toString();
@@ -57,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 display += textView.getText().toString();
                 currentOperator = textView.getText().toString();
                 updateScreen();
+                getResult();
             }
         }
         catch (Exception e){
@@ -64,18 +106,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-    public boolean isLastCharacterContainOperator(){
-        String lastCharacter = display.substring(display.length()-1);
-        if (lastCharacter.equals("+") || lastCharacter.equals("-") ||
-                lastCharacter.equals("×") || lastCharacter.equals("÷") || display.equals("")){
-
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
     public void onClickPoint(View view) {
         if (!display.contains(".")){
             TextView textView = (TextView) view;
@@ -94,31 +124,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-    public double operate(String numberOne, String numberTwo, String operator){
-        switch (operator){
-            case "+":
-                return Double.valueOf(numberOne)+Double.valueOf(numberTwo);
-            case "-":
-                return Double.valueOf(numberOne)-Double.valueOf(numberTwo);
-            case "×":
-                return Double.valueOf(numberOne)*Double.valueOf(numberTwo);
-            case "÷":
-                return Double.valueOf(numberOne)/Double.valueOf(numberTwo);
-        }
-        return 0;
-    }
 
-    public void onClickEqual(View view) {
-        String[]operation = display.split(Pattern.quote(currentOperator));
-        String firstNumber = operation[0];
-        String secondNumber = operation[operation.length-1];
-        if (result >0){
-            result = operate(String.valueOf(result),secondNumber,currentOperator);
-        }
-        else {
-            result = operate(operation[0],operation[1],currentOperator);
-        }
-        resultTV.setText(String.valueOf(result));
-        Toast.makeText(this, ""+operation[operation.length-1], Toast.LENGTH_SHORT).show();
-    }
 }
